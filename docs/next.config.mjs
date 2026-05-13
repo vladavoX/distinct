73f87833f71309e72893ae8fb14a523ea5e1c5df
Vladimir Aleksic <vlada.pzs@hotmail.com>
2026-05-13 22:58:36 +0200

    revert next config file extension

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -2,7 +2,8 @@ import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
-const config: import("next").NextConfig = {
+/** @type {import('next').NextConfig} */
+const config = {
   reactStrictMode: true,
 };
 
